49920c1aca1239a1af068c445555df6980ccb392
zenitani <206021@std.hi-joho.ac.jp>
2022-11-11 14:11:28 +0900

    トップページの表示とログイン画面の遷移（制作中）

---
+++ src/Toppage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Toppage = () => {
+    return (
+        <div>
+        <h1>preport!</h1>
+    <div>
+            <input name='log' type='submit' value='ログイン'/>
+    </div>
+        </div>
+);
+};
+
+export default Toppage;
