03bff064a1e0407fef3d05e879f1dcba0bbdc4b1
zenitani <206021@std.hi-joho.ac.jp>
2022-11-14 15:11:00 +0900

    トップページの表示、認証用アカウントの登録 ログイン処理の制作

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,14 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    apiKey: "AIzaSyAGe-wnWz7-1O4mNsOqGqMvzH7OT0RsTQI",
+    authDomain: "react-preport.firebaseapp.com",
+    databaseURL: "https://react-preport-default-rtdb.firebaseio.com",
+    projectId: "react-preport",
+    storageBucket: "react-preport.appspot.com",
+    messagingSenderId: "584015182079",
+    appId: "1:584015182079:web:c77bc8e5d1763622ae74ca",
+    measurementId: "G-JVPK2NR97R"
 };
 
 const app = initializeApp(firebaseConfig);
