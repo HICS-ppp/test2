b2ba2ca552b1e97f06a4c275a61c43971f423f1e
yonemura <206042@std.hi-joho.ac.jp>
2022-11-22 15:57:53 +0900

    11/22 パスワード一致確認 米村

--- src/signup/signup.tsx
+++ src/signup/signup.tsx
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Link, Navigate } from "react-router-dom";
 import { ref, push } from "firebase/database";
 import { database } from "../firebase"
 import {getAuth, sendSignInLinkToEmail} from "firebase/auth";
 import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
+import { useForm, SubmitHandler } from "react-hook-form";
 import "./signup.css";
+import {getValue} from "@testing-library/user-event/dist/utils";
 
 
 const Register = () => {
+
+
+    const [value, setValue] = useState(""); // valueをstateで管理
+    const [value1,setValue1] = useState("");
+
+    useEffect(() => {
+
+    },)
 
     /*state変数を定義*/
     /*Realtime Databaseに登録するデータの変数を定義*/
@@ -23,19 +33,49 @@ const Register = () => {
     /* ↓関数「handleSubmit」を定義 */
         const handleSubmit = async (e: any) => {
             e.preventDefault();
-            try {
+
+            if (Pass !== Pass2) {
+                alert("パスワードが一致しません");
+            } else {
+                try {
+                    //　ローカルストレージに一度保存する
+                    localStorage.setItem('N', userName);
+                    localStorage.setItem('M', Email)
+                    localStorage.setItem('P', Pass);
+                    localStorage.setItem('P2', Pass2);
+                    localStorage.setItem('A', Age);
+                    localStorage.setItem('G', Gender);
+                    localStorage.setItem('I', userID);
+                    localStorage.setItem('I2', userID);
+                    window.location.href = "/signup_mail_send"
+                } catch (error) {
+                    alert("正しく入力してください");
+                }
+            }
+        }
+
+/*            try {
                 //　ローカルストレージに一度保存する
                 localStorage.setItem('N',userName);localStorage.setItem('M',Email)
                 localStorage.setItem('P',Pass);localStorage.setItem('P2',Pass2);
                 localStorage.setItem('A',Age);localStorage.setItem('G',Gender);
                 localStorage.setItem('I',userID);localStorage.setItem('I2',userID);
                 // 画面遷移処理
-                window.location.href = "/signup_mail_send"
+                //window.location.href = "/signup_mail_send"
+                if(Pass !== Pass2){
+                    alert("pass正しく入力してください");
+                    return
+                }else {
+                    window.location.href = "/signup_mail_send"
+                }
 
             } catch (error) {
                 alert("正しく入力してください");
-            }
-        };
+            } /!*catch (){
+                alert("正しく入力してください");
+            }*!/
+        };*/
+
     return (
 
         <><>
@@ -44,6 +84,7 @@ const Register = () => {
                 <div className="sum">
                     <label　className="userinput">ユーザID</label>
                     <input
+                        required
                         onChange={(e:any) => setuserID(e.target.value)}
                         className="regbox1"
                         type="text"
@@ -52,6 +93,7 @@ const Register = () => {
                 <div className="sum">
                     <label className="userinput">メールアドレス</label>
                     <input
+                        required
                            className="regbox1"
                            onChange={(e:any) => setEmail(e.target.value)}
                                 name="email"
@@ -60,32 +102,40 @@ const Register = () => {
                 </div>
                 <div className="sum">
                     <label className="userinput">ユーザ名</label>
-                    <input onChange={(e:any) => setUserName(e.target.value)}
+                    <input required
+                        onChange={(e:any) => setUserName(e.target.value)}
                            className="regbox1"
                                 name="username"
                                 type="text"
                                 placeholder="username" />
                 </div>
+
+
                 <div className="sum">
                     <label className="userinput">パスワード</label>
-                    <input
+                    <input required
                             onChange={(e:any) => setPass(e.target.value)}
                             className="regbox1"
                                 name="pass"
                                 type="password"
-                                placeholder="pass" />
+                                placeholder="pass"
+                    />
                 </div>
                 <div className="sum">
                     <label className="userinput">再パスワード</label>
-                    <input onChange={(e:any) => setPass2(e.target.value)}
+                    <input
+                        required
+                        onChange={(e:any) => setPass2(e.target.value)}
                            className="regbox1"
                                 name="pass2"
                                 type="password"
+
                                 placeholder="pass2" />
                 </div>
                 <div className="sum">
                     <label className="userinput">年齢</label>
-                    <input onChange={(e:any) => setAge(e.target.value)}
+                    <input required
+                        onChange={(e:any) => setAge(e.target.value)}
                            className="agebox1"
                                 name="age"
                                 type="number"
@@ -93,17 +143,16 @@ const Register = () => {
                 </div>
                 <div className="sum">
                     <label className="userinput">性別</label>
-                    <input onChange={(e:any) => setGender(e.target.value)}
+                    <input required onChange={(e:any) => setGender(e.target.value)}
                                 name="gender" type="radio" value="男" />男
-                    <input onChange={(e:any) => setGender(e.target.value)}
+                    <input required onChange={(e:any) => setGender(e.target.value)}
                                 name="gender" type="radio" value="女"/>女
                 </div>
                 <div className="sum">
 
                     <button className="register">登録</button>
 
-
-                                {/* ↓リンクを追加 */}
+                    {/* ↓リンクを追加 */}
                 <p>ログインは<Link to={`/login/`}>こちら</Link></p>
                 </div>
             </form>
