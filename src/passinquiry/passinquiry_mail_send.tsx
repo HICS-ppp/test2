95f35c8daaccc0083fa9173d8ac1b235f27c0b15
yonemura <206042@std.hi-joho.ac.jp>
2022-11-30 16:28:39 +0900

    11/30 米村

--- src/passinquiry/passinquiry_mail_send.tsx
+++ src/passinquiry/passinquiry_mail_send.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {database} from "../firebase";
-import {ref, push, getDatabase} from "firebase/database";
+import {ref, push, getDatabase,onValue} from "firebase/database";
 import {getAuth, isSignInWithEmailLink, sendSignInLinkToEmail, signInWithEmailLink} from "firebase/auth";
 import {Link} from "react-router-dom";
 
@@ -8,44 +8,21 @@ import {Link} from "react-router-dom";
 const Passinquirymail = () => {
 
 
-    // ローカルストレージ内データのget
-
     const userid = localStorage.getItem('userid');
-    //ueda
-
-
-/*
-    const [maile,setmaile] = useState<any>("");
+    const [mail, setmail] = useState<any>('');
 
     const db = getDatabase()
-   setmaile(ref(db, "Users/" +userid+ '/mailaddress'))
-*/
+    setmail(ref(db, "Users/" + userid + "/pass"))
+    onValue(mail, (snapshot) => {
+        let aaa = snapshot.val()
+        localStorage.setItem('UserID', aaa)
+    })
+    const dbID = localStorage.getItem('UserID')
 
-/*
-
-
-    // 控えurl:'https://www.example.com/finishSignUp?cartId=1234'
-    // ↓firebaseメール認証の定義処理
-    const actionCodeSettings = {
-        url: 'http://localhost:3000/passchange',
-        handleCodeInApp: true,
-    }
-    const auth = getAuth();
-
-    // @ts-ignore
-   sendSignInLinkToEmail(auth, Maile, actionCodeSettings)
-        .then(() => {
-            // @ts-ignore
-            window.localStorage.setItem('maile', maile);
-        })
-*/
-
-
-    // @ts-ignore
     return(
 
         <form>
-            <h1>{userid}にメールを送信しました</h1>
+            <h1>{dbID}にメールを送信しました</h1>
             <button><Link to={`/`}>トップへ</Link></button>
         </form>
 
