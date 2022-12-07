908f8c04c4126452b5a3a581a12710093bbf18fa
zenitani <206021@std.hi-joho.ac.jp>
2022-12-07 23:54:54 +0900

    グループ作成処理を行った

--- src/group/groupmenu_load.tsx
+++ src/group/groupmenu_load.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {ref, getDatabase, startAt,query, orderByChild, onChildAdded, set} from "firebase/database"
+import {ref, getDatabase, startAt,query, orderByChild, onChildAdded, set,equalTo} from "firebase/database"
 import {database} from "../firebase";
 
 const Groupmenu_load = () => {
 
     const db = getDatabase()
-    const aaa = (query(ref(db,'Groups/'),orderByChild('groupID'),startAt(1)))
+    const serchGroupID = (query(ref(db,'Groups/'),orderByChild('groupID'),startAt(1)))
     const createGroupName = sessionStorage.getItem('groupName')
     const dateObj = new Date()
     // 時間取得
@@ -13,16 +13,27 @@ const Groupmenu_load = () => {
                                     + dateObj.getDate() +' '+ dateObj.getHours() + ':'
                                     + dateObj.getMinutes() +':'+ dateObj.getSeconds())
     const userName = sessionStorage.getItem('SessionUserName')
+    const serchGroupName = (query(ref(db,'Groups/'),orderByChild('createName'),equalTo(createGroupName)))
 
     //上から順番に処理させる処理
     const pr = async () => {
+        await Data_check()
         await Database_set()
         await timeout()
         await groupmenu_success()
     }
 
+    const Data_check = async () => {
+        onChildAdded(serchGroupName,(snapshot => {
+            let xxx = snapshot.val()
+            sessionStorage.setItem('nameCheck',xxx.createName)
+            sessionStorage.setItem('userCheck',xxx.createUser)
+        }))
+    }
+
     const Database_set = async () => {
-        onChildAdded(aaa, (snapshot) => {
+        onChildAdded(serchGroupID, (snapshot) => {
+            // スナップショットの値を変換、取り出す処理
             let vvv = snapshot.val()
             console.log(snapshot.key) //値を見るためのテスト
             console.log(vvv.groupID)  //値を見るためのテスト
@@ -32,15 +43,6 @@ const Groupmenu_load = () => {
             let maxgroupID = Math.max(vvv.groupID)
             // SessionにID + 1　の値をセットしてグループIDにセットする
             sessionStorage.setItem('groupID', String(Number(maxgroupID) + 1))
-        })
-        const createGroupID = sessionStorage.getItem('groupID')
-
-            //RealtimeDatabaseにデータをセットする処理
-        set(ref(database, "Groups/" + 'GR' + createGroupID + "/"), {
-            createName: createGroupName,
-            createTime: createTime,
-            createUser: userName,
-            groupID: createGroupID
         })
     }
 
@@ -54,7 +56,24 @@ const Groupmenu_load = () => {
     }
     //次の画面へ遷移する処理
     const groupmenu_success = async () => {
-        window.location.href = './groupmenu'
+        const createGroupID = sessionStorage.getItem('groupID')
+        const nameCheck = sessionStorage.getItem('nameCheck')
+        const userCheck = sessionStorage.getItem('userCheck')
+        if (nameCheck != createGroupName || userCheck != userName) {
+            //RealtimeDatabaseにデータをセットする処理
+            set(ref(database, "Groups/" + 'GR' + createGroupID + "/"), {
+                createName: createGroupName,
+                createTime: createTime,
+                createUser: userName,
+                groupID: createGroupID
+            })
+            window.location.href = './groupmenu'
+        }else{
+            sessionStorage.removeItem('nameCheck');sessionStorage.removeItem('userCheck');
+            sessionStorage.removeItem('groupName');sessionStorage.removeItem('groupID');
+            alert('同じ名前で作成したグループがあるため作成に失敗しました')
+            window.location.href = '../mainpage'
+        }
     }
     //18行目の順番処理実行
     pr()
