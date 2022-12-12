6f51525d5ed3a9a5e18637c9b788ff358e0e9321
yonemura <206042@std.hi-joho.ac.jp>
2022-12-12 12:07:59 +0900

    12/12 米村

--- src/toppage/toppage.tsx
+++ src/toppage/toppage.tsx
@@ -7,16 +7,16 @@ const Toppage = () => {
     return (
         <>
             <body className="body1">
-
-
-
+            <div className="btndiv">
                 <Link to={`/login/`}>
-
-                        <span className="btn">Preport!</span>
-
+                    <span className="btn">Preport!</span>
                 </Link>
+            </div>
+
+            <div className="sub1div">
                 <h3 className="sub1">presentation support</h3>
 
+            </div>
 
 
             </body>
